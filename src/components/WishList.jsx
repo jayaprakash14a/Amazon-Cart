ca88bd4645b163be4a24df3f1f27f4ced21689fb
Jaya Prakash Aenugutala <jayaprakashaenugutala@gmail.com>
2024-12-16 23:23:00 +0530

    wishlist page completed using recoil.

--- src/components/WishList.jsx
+++ src/components/WishList.jsx
@@ -1,18 +1,35 @@
-import { useRecoilValue } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
 import styling from '../WishList.module.css'
-import { wishlistoptionAtom } from '../store/wishItemsState'
+import { itemsAtomFamily, wishlistoptionAtom } from '../store/wishItemsState'
+import { CartItems } from '../resources/cartItems';
 
 
 function WishList(){
 
     const wishlistoptions = useRecoilValue(wishlistoptionAtom);
-    const wishList = wishlistoptions.map( item => <WishListOption title={item.title} isDefaultList={item.isDefaultList} /> )
+    const wishList = wishlistoptions.map( item => <WishListOption key={item.id} title={item.title} isDefaultList={item.isDefaultList} /> )
+    const wishlistItems = CartItems.map(item => <WishListItem id={item.id} key={item.id}/>);
 
     return(
         <>
             <div className={styling.wishlistlayout}>
                 <div className={styling['wishlist-options']}>
                     {wishList}
+                </div>
+                <div className={styling.wishlistmenu}>
+                    <div className={styling['wishList-banner']}>
+                        <div className={styling.wishlisttitle}>
+                            <h2 className={styling["wishlist-title"]}>Your Wish List</h2>
+                            <h3 className={styling["wishlist-defaulttext"]}>Public</h3>
+                        </div>
+                        <div className={styling.wishlistshare}>
+                            <a href=''>Send list to others</a>
+                            <h3 className={styling['m-0']}>...</h3>
+                        </div>
+                    </div>
+                    <div className={styling['wishlist-items']}>
+                        {wishlistItems}
+                    </div>
                 </div>
             </div>
         </>
@@ -26,6 +43,34 @@ function WishListOption({title, isDefaultList}){
                 <h1 className={styling["wishlist-title"]}>{title}</h1>
                 {isDefaultList && <h3 className={styling["wishlist-defaulttext"]}>Default List</h3>}
             </div>
+        </>
+    )
+}
+
+function WishListItem({id}){
+    const [item, setItem] = useRecoilState(itemsAtomFamily(id));
+
+    function addToCart(){
+        setItem((x)=>({ ...x, AddedToCart: true
+        }))
+    }
+
+    function proceedToCheckout(){
+        setItem((x)=>({ ...x, AddedToCart: false
+        }))
+    }
+
+    return(
+        <>
+        <div className={styling['item-tile']}>
+            <div className={styling['item-details']}>
+                <img src={item.url} alt={item.url} className={styling['item-image']} />
+                <h3 className={styling['m-0']}>{item.name}</h3>
+                <h3 className={styling['m-0']}>₹ {item.price}</h3>
+            </div>
+            {item.AddedToCart ? <button className={styling['success-Btn']} onClick={proceedToCheckout} >Proceed to checkout</button> : <button className={styling['success-Btn']} onClick={addToCart}>Add to Cart</button>}
+            
+        </div>
         </>
     )
 }
--- src/store/wishItemsState.js
+++ src/store/wishItemsState.js
@@ -1,9 +1,18 @@
-import { atom } from "recoil";
+import { atom, atomFamily } from "recoil";
+import { CartItems } from "../resources/cartItems";
 
 export const wishlistoptionAtom = atom({
     key: "wishlistoptionAtom",
     default : [{
+        id: 1,
         title : "Your Wish List",
         isDefaultList : true
     }]
+})
+
+export const itemsAtomFamily = atomFamily({
+    key: "itemsAtomFamily",
+    default : (id) =>{
+        return CartItems.find((item)=> item.id === id);
+    }
 })
